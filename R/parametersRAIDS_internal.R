87c16054b318c8d96ffa46fdd48ace6d765999c2
adeschen <adeschen@hotmail.com>
2026-07-03 19:44:41 -0400

    Update functions related to internal validation of parameters

--- R/parametersRAIDS_internal.R
+++ R/parametersRAIDS_internal.R
@@ -1,78 +1,158 @@
-
-
-
+#' @title Validate the parameters
+#'
+#' @description This function validates the parameters present in the 
+#' \code{parametersRAIDS} object.
+#'
+#' @param parameters a \code{parametersRAIDS} an object with all the RAIDS
+#' parameters
+#'
+#' @return \code{TRUE} when all the parameters in the object are valid; 
+#' otherwise \code{FALSE}
+#'
+#' @examples
+#' ##TODO
+#'
+#'
+#' @author Pascal Belleau and Astrid Deschênes
+#' @encoding UTF-8
+#' @keywords internal
 validateParamRAIDS <- function(parameters) {
-  pRAIDSNames <- c("reference", # HGDP1kg
-                      "studyDF",
-                      "studyDFSyn",
-                      "pedStudy",
-                      "studyType",
-                      "genoSource",
-                      "blockTypeId",
-                      "genome",
-                      "chrInfo",
-                      "profileFile",
-                      "profileFileGeno",
-                      "pathProfileGDS",
-                      "fileReferenceGDS",
-                      "fileReferenceAnnotGDS",
-                      "inferenceType",
-                      "batch",
-                      "prefix",
-                      "nbSim",
-                      "offset",
-                      "minCov",
-                      "minProb",
-                      "seqError",
-                      "seqErrorSyn",
-                      "pRecomb",
-                      "np",
-                      "listPos",
-                      "syntheticRefDF",
-                      "pruningMethod",
-                      "slideWindowMaxBP",
-                      "thresholdLD",
-                      "specificSNV",
-                      "genoType",
-                      "phaseType",
-                      "phase",
-                      "PCAmissingRate",
-                      "PCAalgorithm",
-                      "eigenCount",
-                      "eigenCountSyn",
-                      "kList",
-                      "pcaList",
-                      "fieldPopInRef",
-                      "fieldPopInfAnc",
-                      "fieldSubPop",
-                      "verbose")
-  if (!is.numeric(params$n_iter) || length(params$n_iter) != 1 ||
-      params$n_iter <= 0 || params$n_iter %% 1 != 0) {
-    stop("`n_iter` must be a single positive integer.")
-  }
+    pRAIDSNames <- c("reference", # HGDP1kg
+                                "studyDF",  ## DONE AD
+                                "studyDFSyn",
+                                "pedStudy", ## DONE AD
+                                "studyType",
+                                "genoSource", ## DONE AD
+                                "blockTypeId",
+                                "genome",
+                                "chrInfo", ## DONE AD
+                                "profileFile",
+                                "profileFileGeno",
+                                "pathProfileGDS",
+                                "fileReferenceGDS", ## DONE AD
+                                "fileReferenceAnnotGDS", ## DONE AD
+                                "inferenceType",
+                                "batch",
+                                "prefix",
+                                "nbSim",
+                                "offset",
+                                "minCov",
+                                "minProb",
+                                "seqError",
+                                "seqErrorSyn",
+                                "pRecomb",
+                                "np",
+                                "listPos",
+                                "syntheticRefDF", ##DONE AD
+                                "pruningMethod",
+                                "slideWindowMaxBP",
+                                "thresholdLD",
+                                "specificSNV",
+                                "genoType",
+                                "phaseType",
+                                "phase",
+                                "PCAmissingRate",
+                                "PCAalgorithm",
+                                "eigenCount",
+                                "eigenCountSyn",
+                                "kList",
+                                "pcaList",
+                                "fieldPopInRef",
+                                "fieldPopInfAnc",
+                                "fieldSubPop",
+                                "verbose") ## DONE AD
+    
+    ## All parameters must have an entry
+    if (!all(names(parameters) %in% pRAIDSNames)){ 
+        stop("All parameters must be present: ", 
+                paste0(pRAIDSNames, collapse=", "))
+    }
+  
+    validateParamRAIDS_subpart01(parameters=parameters)
+  
+    validateParamRAIDS_subpart02(parameters=parameters)
+    
+    invisible(TRUE)
+}
 
-  if (!is.numeric(params$alpha) || length(params$alpha) != 1 ||
-      params$alpha <= 0 || params$alpha >= 1) {
-    stop("`alpha` must be a single number between 0 and 1.")
-  }
+#' @title Validate the first subsection fo the input parameters
+#'
+#' @description This function validates a subsection of the parameters 
+#' present in the \code{parametersRAIDS} object.
+#'
+#' @param parameters a \code{parametersRAIDS} an object with all the RAIDS
+#' parameters
+#'
+#' @return \code{TRUE} when all the parameters tested in the object are valid; 
+#' otherwise \code{FALSE}
+#'
+#' @examples
+#' ##TODO
+#'
+#'
+#' @author Pascal Belleau and Astrid Deschênes
+#' @encoding UTF-8
+#' @keywords internal
+validateParamRAIDS_subpart01 <- function(parameters) {
 
-  allowed_models <- c("linear", "logistic", "poisson")
-  if (!is.character(params$model_type) || length(params$model_type) != 1 ||
-      !params$model_type %in% allowed_models) {
-    stop(sprintf("`model_type` must be one of: %s.",
-                  paste(allowed_models, collapse = ", ")))
-  }
+    ## The study data frame must have the mandatory columns
+    validateStudyDataFrameParameter(studyDF=parameters$studyDF)
+  
+    ## The PED study must have the mandatory columns
+    validatePEDStudyParameter(pedStudy=parameters$pedStudy)
 
-  if (!is.character(params$covariates) || length(params$covariates) == 0) {
-    stop("`covariates` must be a non-empty character vector.")
-  }
+    ## The genoSource must be a character string
+    if(!(is.character(parameters$genoSource))) {
+        stop("The \'genoSource\' parameter must be a character string.")
+    }
 
-  if (!is.character(params$output_dir) || length(params$output_dir) != 1) {
-    stop("`output_dir` must be a single string.")
-  }
+    ## The chrInfo must be a vector of integer
+    validatePositiveIntegerVector(parameters$chrInfo, "chrInfo")
+  
+    ## The syntheticRefDF must have the mandatory columns
+    validateDataRefSynParameter(syntheticRefDF=parameters$syntheticRefDF)
+  
+    ## The fileReferenceGDS must be a character string and the file must exists
+    if (!(is.character(fileReferenceGDS) && (file.exists(fileReferenceGDS)))) {
+        stop("The \'fileReferenceGDS\' must be a character string ",
+                "representing the Reference GDS file. The file must exist.")
+    }
+  
+    ## The fileReferenceAnnotGDS must be a character string and
+    ## the file must exist
+    if (!(is.character(fileReferenceAnnotGDS) &&
+                (file.exists(fileReferenceAnnotGDS)))) {
+        stop("The \'fileReferenceAnnotGDS\' must be a character string ",
+                "representing the Reference Annotation GDS file. ",
+                "The file must exist.")
+    }
 
-  invisible(TRUE)
+    invisible(TRUE)
 }
 
 
+#' @title Validate the second subsection fo the input parameters
+#'
+#' @description This function validates a subsection of the parameters 
+#' present in the \code{parametersRAIDS} object.
+#'
+#' @param parameters a \code{parametersRAIDS} an object with all the RAIDS
+#' parameters
+#'
+#' @return \code{TRUE} when all the parameters tested in the object are valid; 
+#' otherwise \code{FALSE}
+#'
+#' @examples
+#' ##TODO
+#'
+#'
+#' @author Pascal Belleau and Astrid Deschênes
+#' @encoding UTF-8
+#' @keywords internal
+validateParamRAIDS_subpart02 <- function(parameters) {
+
+    validateLogical(parameters$verbose, "verbose")
+
+    invisible(TRUE)
+}
 
