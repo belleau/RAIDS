08a65eaa10e1becb2126e9f9e2b9760d9b81e47c
adeschen <adeschen@hotmail.com>
2026-07-07 13:03:45 -0400

    Update doc for computePCARefRMMulti1() function

--- R/ancestryAnalysis_internal.R
+++ R/ancestryAnalysis_internal.R
@@ -161,39 +161,11 @@ createProfile2 <- function(pRAIDS) {
 #' for a list of pruned SNVs present in a Profile GDS file. The
 #' \link[SNPRelate]{snpgdsPCA} function is used to do the calculation.
 #'
-#' @param gdsProfile an object of class \link[SNPRelate]{SNPGDSFileClass},
-#' the opened Profile GDS file.
-#'
-#' @param refProfileIDs a \code{vector} of reference 1KG profile identifiers
-#' that are present in the Profile GDS file.
-#' Those profiles minus the one present in the \code{listRM} vector will be
-#' used to run the PCA analysis.
-#'
 #' @param listRM a \code{vector} of \code{character} strings containing the
 #' identifiers for the reference samples that need to be removed for the
 #' PCA analysis.
 #'
-#' @param np a single positive \code{integer} representing the number of CPU
-#' that will be used. Default: \code{1L}.
-#'
-#' @param algorithm a \code{character} string representing the algorithm used
-#' to calculate the PCA. The 2 choices are "exact" (traditional exact
-#' calculation) and "randomized" (fast PCA with randomized algorithm
-#' introduced in Galinsky et al. 2016). Default: \code{"exact"}.
-#'
-#' @param eigenCount a single \code{integer} indicating the number of
-#' eigenvectors that will be in the output of the \link[SNPRelate]{snpgdsPCA}
-#' function; if 'eigenCount' <= 0, then all eigenvectors are returned.
-#' Default: \code{32L}.
-#'
-#' @param missingRate a \code{numeric} value representing the threshold
-#' missing rate at with the SNVs are discarded; the SNVs are retained in the
-#' \link[SNPRelate]{snpgdsPCA} function
-#' with "<= missingRate" only; if \code{NaN}, no missing threshold.
-#' Default: \code{0.025}.
-#'
-#' @param verbose a \code{logical} indicating if message information should be
-#' printed.
+#' @param pRAIDS a TODO.
 #'
 #' @return a \code{list} containing 2 entries:
 #' \describe{
@@ -212,44 +184,11 @@ createProfile2 <- function(pRAIDS) {
 #'
 #' @examples
 #'
-#' ## Required library
-#' library(SNPRelate)
-#'
-#' ## Load the known ancestry for the demo 1KG reference profiles
-#' data(demoKnownSuperPop1KG)
-#'
-#' # The name of the synthetic study
-#' studyID <- "MYDATA.Synthetic"
-#'
-#' ## Profiles that should be removed from the PCA analysis
-#' ## Those profiles has been used to generate the synthetic data set
-#' samplesRM <- c("HG00246", "HG00325", "HG00611", "HG01173", "HG02165",
-#'     "HG01112", "HG01615", "HG01968", "HG02658", "HG01850", "HG02013",
-#'     "HG02465", "HG02974", "HG03814", "HG03445", "HG03689", "HG03789",
-#'     "NA12751", "NA19107", "NA18548", "NA19075", "NA19475", "NA19712",
-#'     "NA19731", "NA20528", "NA20908")
-#'
-#' ## Path to the demo Profile GDS file is located in this package
-#' dataDir <- system.file("extdata/demoKNNSynthetic", package="RAIDS")
-#'
-#' ## Open the Profile GDS file
-#' gdsProfile <- snpgdsOpen(file.path(dataDir, "ex1.gds"))
-#'
-#' ## Compute PCA for the 1KG reference profiles excluding
-#' ## the profiles used to generate the synthetic profiles
-#' results <- RAIDS:::computePCARefRMMulti(gdsProfile=gdsProfile,
-#'     refProfileIDs=names(demoKnownSuperPop1KG), listRM=samplesRM, np=1L,
-#'     algorithm="exact", eigenCount=32L, missingRate=0.025, verbose=FALSE)
-#'
-#' ## The PCA on the pruned SNVs data set for selected profiles
-#' head(results$pca.unrel$eigenvect)
-#'
-#' ## Close Profile GDS file (important)
-#' closefn.gds(gdsProfile)
+#' ## TODO
 #'
 #' @author Pascal Belleau, Astrid Deschênes and Alexander Krasnitz
-#' @importFrom gdsfmt read.gdsn index.gdsn
-#' @importFrom SNPRelate snpgdsPCA
+#' @importFrom gdsfmt read.gdsn index.gdsn openfn.gds
+#' @importFrom SNPRelate snpgdsPCA snpgdsPCASNPLoading
 #' @encoding UTF-8
 #' @keywords internal
 computePCARefRMMulti1 <- function(listRM, pRAIDS) {
@@ -259,14 +198,15 @@ computePCARefRMMulti1 <- function(listRM, pRAIDS) {
     # unrelatedSamples <- refProfileIDs[which(!(refProfileIDs %in% listRM))]
 
     fileGDSProfile <- file.path(pRAIDS$pathProfileGDS,
-                                paste0(pRAIDS$pedStudy$Name.ID[1], ".gds"))
+                                    paste0(pRAIDS$pedStudy$Name.ID[1], ".gds"))
     gdsProfile <- openfn.gds(filename=fileGDSProfile)
     gdsReference <- snpgdsOpen(filename=pRAIDS$fileReferenceGDS)
     unrelatedSamples <- read.gdsn(index.gdsn(gdsReference, "sample.id"))[
-            read.gdsn(index.gdsn(gdsReference, "sample.ref")) == 1
-        ]
+            read.gdsn(index.gdsn(gdsReference, "sample.ref")) == 1]
+    
     if(! is.null(listRM)){
-        unrelatedSamples <- unrelatedSamples[which(!(unrelatedSamples %in% listRM))]
+        unrelatedSamples <- unrelatedSamples[which(!(unrelatedSamples %in% 
+                                                                    listRM))]
     }
     listPCA <- list()
 
@@ -282,9 +222,9 @@ computePCARefRMMulti1 <- function(listRM, pRAIDS) {
                                         eigen.cnt=pRAIDS$eigenCount,
                                         verbose=pRAIDS$verbose)
     listPCA[["snp.load"]] <- snpgdsPCASNPLoading(listPCA[["pca.unrel"]],
-                                                 gdsobj=gdsReference,
-                                                 num.thread=pRAIDS$np,
-                                                 verbose=pRAIDS$verbose)
+                                                    gdsobj=gdsReference,
+                                                    num.thread=pRAIDS$np,
+                                                    verbose=pRAIDS$verbose)
     closefn.gds(gdsReference)
     closefn.gds(gdsProfile)
     return(listPCA)
